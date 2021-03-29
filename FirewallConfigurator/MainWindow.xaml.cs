ca7a1371a8d3f68132af74f8a40bdd795e4d2301
Stanislav Chvila <stanislav.chvila@thermofisher.com>
2021-03-29 07:58:57 +0200

    cfg no dhcp, notify property changed added

--- FirewallConfigurator/MainWindow.xaml.cs
+++ FirewallConfigurator/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,31 +14,85 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using JetBrains.Utility;
 
 namespace FirewallConfigurator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
         public MainWindow()
         {
-            Status = "Connecting";
             InitializeComponent();
+            Loaded += MainWindow_Loaded;
         }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= MainWindow_Loaded;
+
+            // get configuration
+            Status = "Connecting";
+            IsDPHC = false;
+            UpdateUIOnConfig();
+
+        }
+
+        public bool IsDPHC { get; set; }
         public string IpText { get; set; }
-        public string Status { get; set; }
+
+        private string _status;
+
+        public string Status
+        {
+            get => _status; 
+            set
+            {
+                _status = value;
+                RaisePropertyChanged(nameof(Status));
+            }
+        }
+        public string Port { get; set; }
+        public string Server { get; set; }
+        public string Gateway { get; set; }
 
         private void typeDhcp_Click(object sender, RoutedEventArgs e)
         {
-            gridStatic.Visibility = Visibility.Hidden;
+            //gridStatic.Visibility = Visibility.Hidden;
+            IsDPHC = true;
+            UpdateUIOnConfig();
 
         }
 
         private void typeStaticIP_Click(object sender, RoutedEventArgs e)
         {
-            gridStatic.Visibility = Visibility.Visible;
+            //gridStatic.Visibility = Visibility.Visible;
+            IsDPHC = false;
+            UpdateUIOnConfig();
+
+        }
+        private void UpdateUIOnConfig()
+        {
+            if (IsDPHC)
+            {
+                gridStatic.Visibility = Visibility.Hidden;
+                typeDhcp.IsChecked = true;
+                typeStaticIP.IsChecked = false;
+                //typeStaticIP.Click += typeStaticIP_Click;
+            }
+            else
+            {
+                gridStatic.Visibility = Visibility.Visible;
+                typeDhcp.IsChecked = false;
+                typeStaticIP.IsChecked = true;
+            }
         }
     }
 }
